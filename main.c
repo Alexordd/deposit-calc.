700ec25ba81a757267c6d32b26c6e4ed58d4e2d4
Alexandr <ren2010rap@mail.ru>
2017-02-20 20:55:20 +0700

    commit 2(if the deposit less than 100)

--- main.c
+++ main.c
@@ -4,9 +4,23 @@
 int main(){
 int d,v,s;
 
-printf("Enter the deposit amount:");
+printf("Enter the deposit amount(thousand of rubles):");
 scanf("%d",&v);
 
 printf("Enter the term(days):");
 scanf("%d",&d);
+
+if (v<100){
+    if(d<=30){
+	s=v*0.9;
+    }
+    if(d>30 && d<121){
+	s=v*1.02;
+    }
+    if(d>120 && d<241){
+	s=v*1.06;
+    }
+    if(d>240 && d<=365){
+	s=v*1.12;
+    }
 
