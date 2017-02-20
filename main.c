226e7470bc5d33e20063d98c98d01269a16e8745
Alexandr <ren2010rap@mail.ru>
2017-02-20 21:35:59 +0700

    commit 5(data checking and completed)

--- main.c
+++ main.c
@@ -10,6 +10,19 @@ scanf("%d",&v);
 printf("Enter the term(days):");
 scanf("%d",&d);
 
+if(d>365){
+    printf("ERROR:Term deposit-no more than 365 days!");
+    return 0;
+    }
+if(d<0){
+    printf("ERROR:Term deposit can't be less than one day!");
+    return 0;
+    }
+if(v<10){
+    printf("ERROR:Minimum deposit amount-10 thousand rubles!");
+    return 0;
+    }
+
 if (v<=100){
     if(d<=30){
 	s=v*0.9;
